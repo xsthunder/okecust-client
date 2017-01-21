f18a2f013bd939ff24850c656f2415f253ce0ee4
xs-pc2 <xsthunder@outlook.com>
2017-01-22 06:44:09 +0800

    优化了上传单个题目不能变课程的问题。 优化了界面

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ angular
         'admin',
         'login',
         'student',
-        'teacher'])
+        'teacher',
+    ])
     .config(function ($urlRouterProvider) {
         $urlRouterProvider.otherwise('/teacher');
     })
--- app/teacher/dialog.ctrl.js
+++ app/teacher/dialog.ctrl.js
@@ -1,3 +1,32 @@
-/**
- * Created by xs on 1/19.
- */
+function ctrl($scope, $log,$mdDialog, headerFactory, TeacherCourse, teacherFactory) {
+    var showAlert = function(title,message) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        // Modal dialogs should fully cover application
+        // to prevent interaction outside of dialog
+        $mdDialog.show(
+            $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+                .title(title)
+                .textContent(message)
+                .ariaLabel('Alert Dialog Demo')
+                .ok('明白')
+        );
+    };
+    var showConfirm = function() {
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+            .title('Would you like to delete your debt?')
+            .textContent('All of the banks have agreed to forgive you your debts.')
+            .ariaLabel('Lucky day')
+            .ok('确定')
+            .cancel('取消');
+
+        $mdDialog.show(confirm).then(function() {
+            $scope.status = 'You decided to get rid of your debt.';
+        }, function() {
+            $scope.status = 'You decided to keep your debt.';
+        });
+    };
+
+}
