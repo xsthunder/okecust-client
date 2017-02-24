ea3f2c72031793d0eff795a43a150bcd12062380
xs-pc2 <xsthunder@outlook.com>
2017-02-24 18:28:23 +0800

    优化显示问答

--- app/teacher/qa/qaReport/qa.report.ctrl.js
+++ app/teacher/qa/qaReport/qa.report.ctrl.js
@@ -2,104 +2,110 @@
  * Created by YY on 2016/11/12.
  */
 (function () {
-  angular.module('teacher.qa.report')
-    .controller('qaReportHeaderCtrl', headerCtrl)
-    .controller('qaReportMainCtrl', mainCtrl)
-  function headerCtrl() {
-    var header = this;
-    header.title = '测试报告';
-  }
+    angular.module('teacher.qa.report')
+        .controller('qaReportHeaderCtrl', headerCtrl)
+        .controller('qaReportMainCtrl', mainCtrl)
+    function headerCtrl() {
+        var header = this;
+        header.title = '测试报告';
+    }
 
-  function mainCtrl($scope,Account, teacherQuizFactory, $log, teacherQaFactory) {
-    $log.log(teacherQuizFactory.nowQuiz);
-    try {
-        var quizId = teacherQuizFactory.nowQuiz._id;
-    }
-    catch (err){
-        Account.showAlert('生成数据失败，请再试并耐心等候');
-      return;
-    }
-    teacherQaFactory.getQuizReport(quizId, function (error, res) {
-      if (!error) {
-        $log.log(res.reports);
-        $scope.reports = res.reports;
-        $scope.status=false;
-        $scope.charts = getCharts(res.reports,function () {
-            // $scope.status=true;
-        })
-      }
-    });
-    function getCharts(reports,callback) {
-      var charts = [];
-      $log.log(reports);
-      for (var i = 0; i < reports.length; i++) {
-        var report = reports[i];
-        var chart = {};
-        chart.type = "PieChart";
-        chart.data = {
-          "cols": [
-            {id: '1', label: '题号', type: 'string'},
-            {id: '2', label: '回答人数', type: 'number'}]
-          // "rows": [
-          //   {c: [{v: 'A'}, {v: report[0]}]},
-          //   {c: [{v: 'B'}, {v: report[1]}]},
-          //   {c: [{v: 'C'}, {v: report[2]}]},
-          //   {c: [{v: 'D'}, {v: report[3]}]},
-          // ]
-        };
-          chart.data.rows=[];
-        for(var j =0 ;j<report.length;j++){
-            chart.data.rows.push(
-              {c: [{v: String.fromCharCode('A'.charCodeAt(0)+j)}, {v: report[j]}]}
-          )
+    function mainCtrl($scope,$state, Account, teacherQuizFactory, $log, teacherQaFactory) {
+        var showAlert = Account.showToast;
+        $log.log(teacherQuizFactory.nowQuiz);
+        try {
+            var quizId = teacherQuizFactory.nowQuiz._id;
         }
-        chart.options = {
-          'title': ('第'+(i+1)+'题报告')
-        };
-        charts.push(chart);
-      }
-      callback();
-      return charts;
-    }
+        catch (err) {
+            showAlert('', '生成数据失败，重试并耐心等候');
+            $state.go('teacher.qa');
+            return;
+        }
+        teacherQaFactory.getQuizReport(quizId, function (error, res) {
 
-    //数据图
-    $scope.myChartObject = {};
-    $scope.myChartObject.type = "PieChart";
-    $scope.onions = [
-      {v: "Onions"},
-      {v: 3},
-    ];
-    $scope.myChartObject.data = {
-      "cols": [
-        {id: "t", label: "Topping", type: "string"},
-        {id: "s", label: "Slices", type: "number"}
-      ], "rows": [
-        {
-          c: [
-            {v: "Mushrooms"},
+            if (error) {
+                showAlert('', '获取数据失败');
+
+            }
+            $log.log(res.reports);
+            $scope.reports = res.reports;
+            $scope.status = false;
+            $scope.charts = getCharts(res.reports, function () {
+                $scope.status = 'finished';
+            })
+
+        });
+        function getCharts(reports, callback) {
+            var charts = [];
+            $log.log(reports);
+            for (var i = 0; i < reports.length; i++) {
+                var report = reports[i];
+                var chart = {};
+                chart.type = "PieChart";
+                chart.data = {
+                    "cols": [
+                        {id: '1', label: '题号', type: 'string'},
+                        {id: '2', label: '回答人数', type: 'number'}]
+                    // "rows": [
+                    //   {c: [{v: 'A'}, {v: report[0]}]},
+                    //   {c: [{v: 'B'}, {v: report[1]}]},
+                    //   {c: [{v: 'C'}, {v: report[2]}]},
+                    //   {c: [{v: 'D'}, {v: report[3]}]},
+                    // ]
+                };
+                chart.data.rows = [];
+                for (var j = 0; j < report.length; j++) {
+                    chart.data.rows.push(
+                        {c: [{v: String.fromCharCode('A'.charCodeAt(0) + j)}, {v: report[j]}]}
+                    )
+                }
+                chart.options = {
+                    'title': ('第' + (i + 1) + '题报告')
+                };
+                charts.push(chart);
+            }
+            callback();
+            return charts;
+        }
+
+        //数据图
+        $scope.myChartObject = {};
+        $scope.myChartObject.type = "PieChart";
+        $scope.onions = [
+            {v: "Onions"},
             {v: 3},
-          ]
-        },
-        {c: $scope.onions},
-        {
-          c: [
-            {v: "Olives"},
-            {v: 31}
-          ]
-        },
-        {
-          c: [
-            {v: "Zucchini"},
-            {v: 1},
-          ]
-        },
-        {
-          c: [
-            {v: "Pepperoni"},
-            {v: 2},
-          ]
-        }
-      ]
-    };
-  }
+        ];
+        $scope.myChartObject.data = {
+            "cols": [
+                {id: "t", label: "Topping", type: "string"},
+                {id: "s", label: "Slices", type: "number"}
+            ], "rows": [
+                {
+                    c: [
+                        {v: "Mushrooms"},
+                        {v: 3},
+                    ]
+                },
+                {c: $scope.onions},
+                {
+                    c: [
+                        {v: "Olives"},
+                        {v: 31}
+                    ]
+                },
+                {
+                    c: [
+                        {v: "Zucchini"},
+                        {v: 1},
+                    ]
+                },
+                {
+                    c: [
+                        {v: "Pepperoni"},
+                        {v: 2},
+                    ]
+                }
+            ]
+        };
+    }
 })();
