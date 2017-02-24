1c3bc814af2deb45eb2f95c9e08d7ebab94b60c6
xs-pc2 <xsthunder@outlook.com>
2017-02-24 20:19:08 +0800

    updated express config

--- expressApp.js
+++ expressApp.js
@@ -1,5 +1,4 @@
 var express =require('express');
 var app=express();
-app.use('/',express.static(__dirname));
-app.use('/acm',express.static('/home/ubuntu/acm'));
+app.use('/*',express.static(__dirname));
 app.listen(80);
