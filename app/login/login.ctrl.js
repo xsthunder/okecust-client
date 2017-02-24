9b2c7b30726e550d9dd9d9b0aa71dec1d1a6098c
xs-pc2 <xsthunder@outlook.com>
2017-02-24 15:03:15 +0800

    优化欢迎toast

--- app/login/login.ctrl.js
+++ app/login/login.ctrl.js
@@ -2,41 +2,43 @@
  * Created by YY on 2016/11/28.
  */
 (function () {
-  angular.module('login')
-    .controller('loginCtrl', ctrl);
-  function ctrl($scope,$mdDialog, $location, Account, $log, $state) {
-      var showAlert = Account.showToast;
+    angular.module('login')
+        .controller('loginCtrl', ctrl);
+    function ctrl($scope, $mdDialog, $location, Account, $log, $state) {
+        var showAlert = Account.showToast;
 
-      $scope.username = '';
-    $scope.password = '';
-    $scope.login = function () {
-      if ('' === $scope.username || '' === $scope.password) {
-        return showAlert('错误',"输入不能为空");
-      }
-      Account.login($scope.username, $scope.password, function (err, res) {
-        if (null !== err) {return showAlert("无法登陆",'请检查账号或密码');}
-        Account.setCredit(res);
-        var type = res.type;
-        console.log(res);
-          Account.getProfile(function (err, profile) {
-              if (err) {
-                  return $log.error(err);
-              }
-              Account.showToast('chengong','欢迎'+profile.name+'老师');
-          });
-        if (type == 7) {
-          $location.path('/admin');
-        } else if (type == 1) {
-          $log.info('student');
-          $state.go('student');
-        } else if (type == 2) {
-          $location.path('/teacher/class');
+        $scope.username = '';
+        $scope.password = '';
+        $scope.login = function () {
+            if ('' === $scope.username || '' === $scope.password) {
+                return showAlert('错误', "输入不能为空");
+            }
+            Account.login($scope.username, $scope.password, function (err, res) {
+                if (null !== err) {
+                    return showAlert("无法登陆", '请检查账号或密码');
+                }
+                Account.setCredit(res);
+                var type = res.type;
+                console.log(res);
+                Account.getProfile(function (err, profile) {
+                    if (err) {
+                        return $log.error(err);
+                    }
+                    Account.showToast('chengong', '欢迎' + profile.name+(type==1?'同学':'老师'));
+                });
+                if (type == 7) {
+                    $location.path('/admin');
+                } else if (type == 1) {
+                    $log.info('student');
+                    $state.go('student');
+                } else if (type == 2) {
+                    $location.path('/teacher/class');
+                }
+            });
+        };
+        $scope.showQR = false;
+        $scope.switchQR = function () {
+            $scope.showQR = !$scope.showQR;
         }
-      });
-    };
-    $scope.showQR = false;
-    $scope.switchQR = function() {
-      $scope.showQR = !$scope.showQR;
     }
-  }
 })();
