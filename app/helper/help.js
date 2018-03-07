9d1e15122d31b858d4dc13892ac241e086fb5fdc
xsthunder <xsthunder@outlook.com>
2018-03-07 23:56:22 +0800

    display

--- app/helper/help.js
+++ app/helper/help.js
@@ -4,7 +4,7 @@
 angular.module('helper', [])
     .factory('AppConstants', function () {
         var self = {};
-        var host = 'ok.lovecust.com:20161';
+        var host = 'http://123.207.188.142:20161';
         if (-1 === location.host.indexOf('okecust')) {
             // Local host mode.
             //host = '127.0.0.1:20161';
